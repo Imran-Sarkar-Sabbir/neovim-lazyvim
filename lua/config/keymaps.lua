412864db88f2b7837b49c4f2317977e3ac724ea7
Imran-Sarar-Sabbir <sabbir.imransarkar@gmail.com>
2024-09-15 00:05:08 +0600

    initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,11 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps hereby
+
+-- Add these lines to your `custom/keybinding.lua` file
+
+-- Map `hjkl` keys for navigation in insert mode
+vim.api.nvim_set_keymap("i", "<C-h>", "<Left>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<C-j>", "<Down>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<C-k>", "<Up>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<C-l>", "<Right>", { noremap = true, silent = true })
---
+++ lua/plugins/lsp_config.lua
@@ -0,0 +1,65 @@
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "lua_ls",
+    "rust_analyzer",
+    "ts_ls",
+  },
+})
+
+vim.keymap.set("n", "<leader>xx", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+
+-- disabled inlayHint functionality
+vim.lsp.handlers["textDocument/inlayHint"] = function() end
+
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pyright = {},
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "jose-elias-alvarez/typescript.nvim",
+      init = function()
+        require("lazyvim.util").lsp.on_attach(function(_, buffer)
+          vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+          vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
+        end)
+      end,
+    },
+    opts = {
+      servers = {
+        tsserver = {
+          on_attach = function(client, bufnr)
+            -- Disable tsserver formatting if you are using prettier
+            client.server_capabilities.documentFormattingProvider = false
+            client.server_capabilities.documentRangeFormattingProvider = false
+          end,
+        },
+      },
+      setup = {
+        tsserver = function(_, opts)
+          require("typescript").setup({ server = opts })
+          return true
+        end,
+      },
+    },
+  },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local null_ls = require("null-ls")
+
+      opts.sources = {
+        -- Eslint
+        null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.formatting.prettier,
+      }
+    end,
+  },
+}
