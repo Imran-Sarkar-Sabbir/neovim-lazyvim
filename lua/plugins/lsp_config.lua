5b85363ea8b4c24f49af1ebb8e45f4141573992f
Imran-Sarar-Sabbir <sabbir.imransarkar@gmail.com>
2024-09-25 22:48:21 +0600

    fixed eslint

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -27,6 +27,7 @@ return {
         require("lazyvim.util").lsp.on_attach(function(_, buffer)
           vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
           vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
+          vim.keymap.set("n", "<leader>xx", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
         end)
       end,
     },
@@ -37,7 +38,6 @@ return {
             -- Disable tsserver formatting if you are using prettier
             client.server_capabilities.documentFormattingProvider = false
             client.server_capabilities.documentRangeFormattingProvider = false
-            vim.keymap.set("n", "<leader>xx", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
           end,
         },
       },
